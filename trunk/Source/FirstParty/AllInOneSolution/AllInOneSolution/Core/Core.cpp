f904d1f8e167ee6407abb3c239d75558e7586575
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-11-10 07:01:41 +0000

    Die Funktion sollte meiner Ansicht nach Teil der WindowKlasse sein, So lange die nicht da ist belasse ich sie in der Main datei, sollte Die klasse da Sein werde ich sie integrieren.
    Koschi

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Core.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Core.cpp
@@ -1,7 +1,32 @@
+#include <iostream>
+#include <fstream>
 
+/// This Function should be a part of Windowclass
+bool LoadConfigFile(std::string FileName);
 
 int main(int argc, char* argv[])
 {
+	const std::string CONFIGFILE = "Config.ini";
+	bool Succesfull = LoadConfigFile(CONFIGFILE);
 	return 0;
 }
 
+bool LoadConfigFile(std::string FileName)
+{
+	/// Placeholder for Members of the Windowclass
+    int ScreenSizeX = 0;
+    int ScreenSizeY = 0;
+    bool IsFullScreen = false;
+
+    std::ifstream ConfigFile;
+    ConfigFile.open(FileName, std::ios_base::out);
+    if(ConfigFile)
+    {
+        if(ConfigFile >> ScreenSizeX >> ScreenSizeY >> IsFullScreen)
+        {
+            ConfigFile.close();
+            return true;
+        }   
+    }
+    return false;
+}
