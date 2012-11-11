133920566a2f74d3d3626448a9b9d84db0331bc8
aeqwaa <aeqwaa@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-11-11 12:23:49 +0000

    Added State Managing System

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/StateManager.cpp
@@ -0,0 +1,54 @@
+#include "StateManager.hpp"
+
+
+StateManager::StateManager() : m_index(0)
+{
+}
+
+
+StateManager::~StateManager()
+{
+}
+
+void StateManager::push(std::unique_ptr<State> state)
+{
+    if(!m_states.empty())
+    {
+        m_states.push_back(std::move(state));
+        m_states.back()->initialize();
+        m_states.back()->pause();
+    }
+    else
+    {
+        m_states.push_back(std::move(state));
+        m_states.back()->initialize();
+    }
+}
+
+void StateManager::next()
+{
+    if(m_states[m_index] != m_states.back())
+    {
+        m_index++;
+    }
+}
+
+void StateManager::prev()
+{
+    if(m_states[m_index] != m_states.front())
+    {
+        m_index--;
+    }
+}
+
+void StateManager::update()
+{
+    if(!m_states.empty())
+        m_states[m_index]->update();
+}
+
+void StateManager::draw()
+{
+    if(!m_states.empty())
+        m_states[m_index]->draw();
+}
