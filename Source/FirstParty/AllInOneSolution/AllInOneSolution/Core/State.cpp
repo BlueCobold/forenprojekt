978bbd6071a7ae021566bc115a7984faa9f0ae2e
aeqwaa <aeqwaa@gmail.com>
2012-11-12 19:36:21 +0000

    Improved FPS HUD and improved the code itself.

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State.cpp
@@ -1,7 +1,9 @@
 #include "State.hpp"
 
 
-State::State(sf::RenderWindow& screen) : m_screen(screen), m_pause(false)
+State::State(sf::RenderWindow& screen) 
+    : m_screen(screen),
+    m_pause(false)
 {
 }
 
