3eab7a88b41778f43d7dc0261172b6c21fc96f6b
aeqwaa <aeqwaa@gmail.com>
2012-11-13 15:59:36 +0000

    Improved Entity and added GraphicalObject.hpp and PhysicalObject.hpp

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Entity.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Entity.cpp
@@ -1,11 +1,13 @@
 #include "Entity.hpp"
+#include "Util.hpp"
 
-
-Entity::Entity()
+void Entity::update()
 {
+    m_sprite.setPosition((m_body.GetPosition().x), (m_body.GetPosition().y));
+    m_sprite.setRotation(Util::toDegree<float32, float>(m_body.GetAngle()));
 }
 
-
-Entity::~Entity()
+void Entity::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+    target.draw(m_sprite);
 }
