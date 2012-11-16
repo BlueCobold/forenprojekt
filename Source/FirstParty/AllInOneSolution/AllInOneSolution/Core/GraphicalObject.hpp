f1ce77b4394a4969725d6bcf73b95478272fd9de
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2012-11-16 21:52:04 +0000

    No more spaces in front of brackets in the ResourceManager & a GraphicalObject is now a Drawable and not the Entity itself.

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/GraphicalObject.hpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/GraphicalObject.hpp
@@ -3,17 +3,20 @@
 #ifndef GRAPHICAL_OBJECT_HPP
 #define GRAPHICAL_OBJECT_HPP
 
-#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
 namespace sf
 {
+    class RenderTarget;
     class Texture;
 }
 
 /// This class will be used to give a Object a
 /// binding to a Graphic
-class GraphicalObject
+class GraphicalObject : public sf::Drawable
 {
 public:
 
@@ -29,9 +32,12 @@ public:
     }
 
 protected:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
+
+protected:
     
     sf::Sprite m_sprite;
-    
 };
 
 #endif // GRAPHICAL_OBJECT_HPP
