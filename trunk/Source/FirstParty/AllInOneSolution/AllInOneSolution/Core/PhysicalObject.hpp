ab2f256f28f5bb75ee1d0132a400b7222a9ad5fa
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-11-19 20:15:34 +0000

    PhysicalObject.hpp give this class some new members, new functions bindShape (overloaded) and bind2World
    Ball.hpp / Ball.cpp delete some member (they can found now in PhysicalObject.hpp.

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/PhysicalObject.hpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/PhysicalObject.hpp
@@ -11,18 +11,50 @@ class PhysicalObject
 {
 public:
 
-    PhysicalObject(){}
+    /// Default Constructor extra for Entity class
+    PhysicalObject()
+    {
+        m_body = NULL;
+    }
+    PhysicalObject(enum b2BodyType bodyType, float xPos, float yPos, float density, float friction)
+    {
+        m_body = NULL;
+
+        m_bodyDef.type = bodyType;
+        m_bodyDef.position.Set(xPos, yPos);
+
+        m_fixtureDef.density = density;
+        m_fixtureDef.friction = friction;
+    }
 
     virtual ~PhysicalObject(){}
 
-    void bindBody(b2Body& body)
+    /// Overloaded Function this is for a b2CircleShape
+    void bindShape(b2CircleShape& shape)
     {
-  //       m_body = body;
+        m_fixtureDef.shape = &shape;
+    }
+    /// Overloaded Function this is for a b2PolygonShape
+    void bindShape(b2PolygonShape shape)
+    {
+        m_fixtureDef.shape = &shape;
+    }
+    /// Overloaded Function this is for a b2EdgeShape
+    void bindShape(b2EdgeShape shape)
+    {
+        m_fixtureDef.shape = &shape;
     }
 
+    void bindToWorld(b2World world)
+    {
+        m_body = world.CreateBody(&m_bodyDef); 
+        m_body->CreateFixture(&m_fixtureDef);
+    }
 protected:
 
-   // b2Body m_body;
+    b2Body* m_body;
+    b2BodyDef m_bodyDef;
+    b2FixtureDef m_fixtureDef;
 };
 
 #endif // PHYSICAL_OBJECT_HPP
