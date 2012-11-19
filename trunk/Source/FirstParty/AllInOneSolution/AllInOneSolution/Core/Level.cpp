2c59c7059c0c5fbe68213667b2395574d878cdd9
l.duerrenberger@gmail.com <l.duerrenberger@gmail.com@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-11-19 18:59:12 +0000

    Added the basic structure of the classes Level and LevelManager.

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Level.cpp
@@ -0,0 +1,11 @@
+#include "Level.hpp"
+
+Level::Level()
+{
+
+}
+
+Level::~Level()
+{
+
+}
---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/LevelManager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef LEVEL_MANAGER_HPP
+#define LEVEL_MANAGER_HPP
+
+
+/// Manages the different levels and is
+/// also resposible for loading them.
+class LevelManager
+{
+public:
+    LevelManager();
+    ~LevelManager();
+};
+
+#endif // LEVEL_MANGER_HPP
