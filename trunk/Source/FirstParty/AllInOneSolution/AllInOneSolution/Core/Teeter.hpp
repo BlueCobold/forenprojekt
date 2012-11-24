188f01004a46d2a8b30b2fea2b341a58c3ecc688
aeqwaa <aeqwaa@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-11-24 22:02:47 +0000

    Added Teeter and modified Level::load to load a teeter from a level file Added teeter to 001.lvl

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Teeter.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef TEETER_HPP
+#define TEETER_HPP
+
+#include "Entity.hpp"
+
+/// This class will be used to bind the physical setting and the control to a entity of a teeter
+class Teeter : public Entity
+{
+public:
+
+    Teeter(const float32 x, const float32 y, const float centerX, const float centerY, 
+        const b2FixtureDef& fixtureDef, b2World& world);
+    ~Teeter();
+
+    void update(const float value);
+
+};
+
+#endif //TEETER_HPP
+
