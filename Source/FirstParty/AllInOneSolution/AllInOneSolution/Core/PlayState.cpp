0e6f284a3cc848d3c37e0c634135b41dd6ccbac9
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2012-11-26 16:38:19 +0000

    Removed view movement in PlayState.

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/PlayState.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/PlayState.cpp
@@ -9,10 +9,6 @@ PlayState::PlayState(sf::RenderWindow& screen, ResourceManager& resourceManager)
 {
     screen.setFramerateLimit(60);
     m_level = std::unique_ptr<Level>(new Level(1, m_resourceManager));
-
-    sf::View view = screen.getDefaultView();
-    view.move(-10.f, 0.f);
-    screen.setView(view);
 }
 
 PlayState::~PlayState()
