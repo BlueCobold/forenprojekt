eec4e7f98aec2dc7d040811e01b38aeb993a25ed
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2012-12-01 18:06:28 +0000

    Config load ini-File now to Memory, this correct BUG (The Config values do not get cached and are parsed/read again and again.)

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Config.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/Config.cpp
@@ -3,30 +3,70 @@
 Config::Config(const std::string& fileName) :
     m_fileName(fileName)
 {
-    m_configFile.open(m_fileName, std::ios_base::in);
+    readFile();
 }
 
 Config::~Config(void)
 {
-    m_configFile.close();
+    
 }
 
 std::string Config::eraseOverhang(std::string& data)
 {
-    data.erase(0, data.find("=") + 1); /// Erase key and = in the string
     while(!data.find(" "))             /// Find and erase leading spaces
-            data.erase(0, 1);
+        data.erase(0, 1);
+
+    unsigned int pos = data.find_last_of(" "); 
+    
+    while(pos == data.length() - 1)    /// Find and erase spaces after data
+    {
+        data.erase(pos,pos);
+        pos = data.find_last_of(" ");
+    }
+  
     return data;
 }
 
-bool Config::load(const std::string& fileName)
+bool Config::reload(const std::string& fileName)
 {
     m_fileName = fileName;
+
     if(m_configFile)
         m_configFile.close();
+    
+    return readFile();
+}
+
+bool Config::readFile()
+{
     m_configFile.open(m_fileName, std::ios_base::in);
-    if(m_configFile)
+    if(m_configFile.is_open())
+    {
+        std::string line = "";
+        std::string key = "";
+        std::string value = "";
+        unsigned int pos;
+
+        while(!m_configFile.eof())
+        {
+            std::getline(m_configFile, line);
+
+            pos = line.find('=');
+
+            if(pos == std::string::npos)
+                continue;
+
+            key = line.substr(0, pos-1);
+            eraseOverhang(key);
+            value = line.substr(pos+1);
+            eraseOverhang(value);
+
+            if(!key.empty() && !value.empty())
+                m_content.insert(std::make_pair<std::string&, std::string&>(key, value));
+        }
+        m_configFile.close();
         return true;
-    else
-        return false;
+    }
+    m_configFile.close();
+    return false;
 }
