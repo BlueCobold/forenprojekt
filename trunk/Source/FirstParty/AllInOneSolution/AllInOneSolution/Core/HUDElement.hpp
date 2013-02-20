2d6f7c0f27473574facf7a2c3967a68b9315122f
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-02-20 18:39:24 +0000

    fix HUD Bug when resize Window

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/HUDElement.hpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/HUDElement.hpp
@@ -8,6 +8,11 @@ class ResourceManager;
 /// Basis class for HUD
 class HUDElement
 {
+public:
+    
+    enum VerticalReference {VR_Top, VR_Bottom, VR_Center};
+    enum HorizontalReference {HR_Left, HR_Right, HR_Center};
+
 protected:
 
     LineLabel m_label;
@@ -18,14 +23,19 @@ protected:
 
     sf::Vector2f m_position;
 
+    HorizontalReference m_horizontalReference;
+
+    VerticalReference m_verticalReference;
+
 public:
-    HUDElement(ResourceManager& resourceManager, const sf::Vector2f& position, const float rotation, std::string bitmapFont, std::string text = "");
+    HUDElement(ResourceManager& resourceManager, const sf::Vector2f& position, const float rotation, std::string bitmapFont,
+    HorizontalReference hReference = HR_Left, VerticalReference vReference = VR_Top, std::string text = "");
 
     virtual void update(const DrawParameter& params);
 
     virtual void draw(const DrawParameter& params);
 
-    void setPosition(sf::Vector2f position);
+    void setPosition(sf::Vector2f position, HorizontalReference hReference = HR_Left, VerticalReference vReference = VR_Top);
 
     void setText(std::string text);
 };
