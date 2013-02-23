025237e51278ad663534321702815401d7a7bf23
peter.friedland89@googlemail.com <peter.friedland89@googlemail.com>
2013-02-23 15:15:32 +0000

    [30] Implemented VerticalStripeTransition and set as current Transition for level start.

---
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/rendering/transitions/VerticalStripesTransition.cpp
@@ -0,0 +1,52 @@
+
+#include "VerticalStripesTransition.h"
+
+VerticalStripesTransition::VerticalStripesTransition(
+    const sf::Texture* sourceTexture,
+    const sf::Texture* targetTexture,
+    const int stripeCount,
+    const float duration) :
+    Transition(sourceTexture, targetTexture, duration)
+{
+    if (targetTexture != nullptr)
+    {
+        m_stripeWidth = targetTexture->getSize().x / stripeCount;
+        for(int i = 0; i < stripeCount; ++i)
+        {
+            auto sprite = new sf::Sprite(*targetTexture, sf::IntRect(i * m_stripeWidth, 0, 0, targetTexture->getSize().y));
+            sprite->setPosition(i * m_stripeWidth, 0);
+            m_targetSprites.push_back(std::unique_ptr<sf::Sprite>(sprite));
+        }
+    }
+    if (sourceTexture != nullptr)
+    {
+        m_sourceSprite.setTexture(*sourceTexture, true);
+        m_sourceSprite.setPosition(0, 0);
+    }
+}
+
+void VerticalStripesTransition::update()
+{
+    Transition::update();
+
+    float scale = getProgress();
+    for(auto it = m_targetSprites.begin(); it != m_targetSprites.end(); ++it)
+    {
+        auto currentRect = (*it)->getTextureRect();
+        currentRect.width = static_cast<int>(m_stripeWidth * getProgress());
+        (*it)->setTextureRect(currentRect);
+    }
+}
+
+void VerticalStripesTransition::draw(const DrawParameter& param)
+{
+    if (getSourceTexture() != nullptr)
+        param.getTarget().draw(m_sourceSprite);
+    if (getTargetTexture() != nullptr)
+    {
+        for(auto it = m_targetSprites.begin(); it != m_targetSprites.end(); ++it)
+        {
+            param.getTarget().draw(**it);
+        }
+    }
+}
---
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/rendering/transitions/VerticalStripesTransition.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef VERTICALSTRIPES_TRANSITION_HPP
+#define VERTICALSTRIPES_TRANSITION_HPP
+
+#include "Transition.hpp"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+class VerticalStripesTransition : public Transition
+{
+public:
+    
+    VerticalStripesTransition(
+        const sf::Texture* sourceTexture,
+        const sf::Texture* targetTexture,
+        const int stripeCount,
+        const float duration);
+
+    void update();
+
+    virtual void draw(const DrawParameter& param);
+
+private:
+    sf::Sprite m_sourceSprite;
+    std::vector<std::unique_ptr<sf::Sprite>> m_targetSprites;
+    int m_stripeWidth;
+};
+
+#endif // ALPHA_TRANSITION_HPP
