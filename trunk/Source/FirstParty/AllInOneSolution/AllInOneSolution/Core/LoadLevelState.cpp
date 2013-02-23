58cedbc147c658799f867a8523a46f7602513287
peter.friedland89@googlemail.com <peter.friedland89@googlemail.com@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-02-23 15:15:32 +0000

    [30] Implemented VerticalStripeTransition and set as current Transition for level start.

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/LoadLevelState.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/LoadLevelState.cpp
@@ -1,6 +1,6 @@
 #include "LoadLevelState.hpp"
 #include "resources/ResourceManager.hpp"
-#include "rendering/transitions/AlphaTransition.hpp"
+#include "rendering/transitions/VerticalStripesTransition.h"
 
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Keyboard.hpp>
@@ -24,7 +24,7 @@ LoadLevelState::LoadLevelState(sf::RenderWindow& screen, ResourceManager& resour
     m_level->draw(DrawParameter(m_texture));
     m_texture.display();
 
-    m_fadedScreen = std::unique_ptr<Transition>(new AlphaTransition(nullptr, &m_texture.getTexture(), 0.5f));
+    m_fadedScreen = std::unique_ptr<Transition>(new VerticalStripesTransition(nullptr, &m_texture.getTexture(), 20, 0.5f));
 }
 
 LoadLevelState::~LoadLevelState()
