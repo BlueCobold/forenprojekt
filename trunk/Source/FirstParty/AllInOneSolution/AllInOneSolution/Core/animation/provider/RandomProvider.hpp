7ba0d1122edf4e441f543fc4947567c22c4041f9
qword5E@gmail.com <qword5E@gmail.com@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-03-25 20:20:43 +0000

    Added random values provider

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/animation/provider/RandomProvider.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef RANDOM_PROVIDER_HPP
+#define RANDOM_PROVIDER_HPP
+
+#include "ValueProvider.hpp"
+
+#include <random>
+
+/// Returns a random real number in [min, max)
+class RandomProvider : public ValueProvider
+{
+private:
+
+    const std::uniform_real_distribution<float> m_distribution;
+    std::default_random_engine m_random_engine;
+
+public:
+
+    RandomProvider(const float min, const float max) : m_distribution(min, max)
+    {
+	    m_random_engine.seed(std::random_device()());
+    }
+
+    virtual float getValue()
+    {
+	    return m_distribution(m_random_engine);
+    }
+};
+
+#endif //RANDOM_PROVIDER_HPP
