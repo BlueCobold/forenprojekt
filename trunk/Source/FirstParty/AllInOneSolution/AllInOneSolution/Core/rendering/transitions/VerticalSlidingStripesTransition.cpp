6c50e284fb67e6020303870c02bd5a2aa862bfb5
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-03-25 19:12:52 +0000

    Renamed HorizontalHalvesTransition to HorizontalSlidingStripesTransition add VerticalSlidingStripesTransition

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/rendering/transitions/VerticalSlidingStripesTransition.cpp
@@ -0,0 +1,68 @@
+#include "VerticalSlidingStripesTransition.hpp"
+
+VerticalSlidingStripesTransition::VerticalSlidingStripesTransition(
+    const sf::Texture* sourceTexture,
+    const sf::Texture* targetTexture,
+    const int stripeCount,
+    const float duration) :
+    Transition(sourceTexture, targetTexture, duration)
+{
+    if (targetTexture != nullptr)
+    {
+        m_stripeHeight = targetTexture->getSize().y;
+        m_stripeWidth = targetTexture->getSize().x / stripeCount;
+        for(int i = 0; i < stripeCount; ++i)
+        {
+            auto sprite = new sf::Sprite(*targetTexture, sf::IntRect(i * m_stripeWidth, 0, m_stripeWidth, 0));
+            sprite->setPosition(static_cast<float>(i * m_stripeWidth), 0);
+            m_targetSprites.push_back(std::unique_ptr<sf::Sprite>(sprite));
+        }
+    }
+    if (sourceTexture != nullptr)
+    {
+        m_sourceSprite.setTexture(*sourceTexture, true);
+        m_sourceSprite.setPosition(0, 0);
+    }
+}
+
+void VerticalSlidingStripesTransition::update()
+{
+    Transition::update();
+
+    float scale = getProgress();
+
+    int count = 0;
+    for(auto it = m_targetSprites.begin(); it != m_targetSprites.end(); ++it)
+    {
+        if(count % 2 == 0)
+        {
+            auto currentRect = (*it)->getTextureRect();
+            currentRect.height = static_cast<int>(m_stripeHeight * scale);
+            (*it)->setTextureRect(currentRect);
+        }
+        else
+        {
+            (*it)->setTextureRect(sf::IntRect(static_cast<int>((*it)->getPosition().x),
+                                              m_stripeHeight - static_cast<int>(m_stripeHeight * scale),
+                                              m_stripeWidth,
+                                              static_cast<int>(m_stripeHeight * scale)));
+
+            (*it)->setPosition((*it)->getPosition().x, 
+                               static_cast<float>(m_stripeHeight - static_cast<int>(m_stripeHeight * scale)));
+        }
+        count++;
+    }
+}
+
+void VerticalSlidingStripesTransition::draw(const DrawParameter& param)
+{
+    if (getSourceTexture() != nullptr)
+        param.getTarget().draw(m_sourceSprite);
+    if (getTargetTexture() != nullptr)
+    {
+        for(auto it = m_targetSprites.begin(); it != m_targetSprites.end(); ++it)
+        {
+            param.getTarget().draw(**it);
+        }
+    }
+}
---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/rendering/transitions/VerticalSlidingStripesTransition.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef HORIZONTALHALVES_TRANSITION_HPP
+#define HORIZONTALHALVES_TRANSITION_HPP
+
+#include "Transition.hpp"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+class VerticalSlidingStripesTransition : public Transition
+{
+public:
+    
+    VerticalSlidingStripesTransition(
+        const sf::Texture* sourceTexture,
+        const sf::Texture* targetTexture,
+        const int stripeCount,
+        const float duration);
+
+    void update();
+
+    virtual void draw(const DrawParameter& param);
+
+private:
+    sf::Sprite m_sourceSprite;
+    std::vector<std::unique_ptr<sf::Sprite>> m_targetSprites;
+    int m_stripeWidth;
+    int m_stripeHeight;
+};
+
+#endif // HORIZONTALHALVE_TRANSITION_HPP
