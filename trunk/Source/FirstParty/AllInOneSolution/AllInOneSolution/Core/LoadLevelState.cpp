2281d89733a10e9e5c97188ca0cebed8d7fb8a7b
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-05-08 19:29:41 +0000

    [113] Level loading is separated in a sf::thread

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/LoadLevelState.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/LoadLevelState.cpp
@@ -1,9 +1,9 @@
 #include "LoadLevelState.hpp"
-#include "Utility.hpp"
 
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Graphics/RenderTexture.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include "rendering/transitions/RandomTransition.hpp"
 
 #include <memory>
@@ -13,18 +13,24 @@ LoadLevelState::LoadLevelState(sf::RenderWindow& screen,
                                Config& config) :
     State(screen, resourceManager, config),
     m_level(nullptr),
-    m_lastLevel(nullptr)
+    m_lastLevel(nullptr),
+    m_label("loading", sf::Vector2f(screen.getSize().x / 2.f, screen.getSize().y / 2.f), 0, resourceManager.getBitmapFont("red"), LineLabel::Left),
+    loadingLevelThrerad(&LoadLevelState::loadLevel, this),
+    m_loaded(false),
+    m_loadInProgress(false)
 {
+    m_label.setPosition(m_screen.getSize().x / 2.f - m_label.getWidth() / 2.f, m_screen.getSize().y / 2.f);
 }
 
 LoadLevelState::~LoadLevelState()
 {
-
 }
 
 void LoadLevelState::onEnter(const EnterStateInformation* enterInformation, const float time)
 {
     State::onEnter(enterInformation, time);
+    m_loaded = false;
+    m_loadInProgress = false;
 }
 
 std::unique_ptr<Level> LoadLevelState::gainLevel()
@@ -34,19 +40,54 @@ std::unique_ptr<Level> LoadLevelState::gainLevel()
 
 StateChangeInformation LoadLevelState::update(const float time)
 {
+    int step = static_cast<int>(time * 2) % 4;
+
     updateTime(time);
+
+    if(m_loaded)
+    {
+        m_playStateInfo.m_returnFromPause = false;
+        m_playStateInfo.m_level = m_lastLevel;
+        m_transitionStateInfo.m_level = m_lastLevel;
+        m_transitionStateInfo.m_followingState = PlayStateId;
+        m_transitionStateInfo.m_onEnterInformation = &m_playStateInfo;
+        return StateChangeInformation(TransitionStateId, &m_transitionStateInfo);
+    }
+    
+    if(!m_loadInProgress)
+        loadingLevelThrerad.launch();
+    
+    if(step == 0)
+        m_label.setText("loading");
+    else if(step == 1)
+        m_label.setText("loading.");
+    else if(step == 2)
+        m_label.setText("loading..");
+    else if(step == 3)
+        m_label.setText("loading...");
+
+    return StateChangeInformation::Empty();
+}
+void LoadLevelState::loadLevel()
+{
+    m_loadInProgress = true;
     m_level = std::unique_ptr<Level>(new Level(7, m_resourceManager, m_config));
     m_lastLevel = m_level.get();
-    
-    m_playStateInfo.m_returnFromPause = false;
-    m_playStateInfo.m_level = m_lastLevel;
-    m_transitionStateInfo.m_level = m_lastLevel;
-    m_transitionStateInfo.m_followingState = PlayStateId;
-    m_transitionStateInfo.m_onEnterInformation = &m_playStateInfo;
-
-    return StateChangeInformation(TransitionStateId, &m_transitionStateInfo);
+    m_loaded = true;
 }
 
 void LoadLevelState::draw(const DrawParameter& params)
 {
+    sf::RectangleShape whiteRect;
+    whiteRect.setSize(m_screen.getView().getSize());
+    whiteRect.setFillColor(sf::Color(255, 255, 255, 255));
+    params.getTarget().draw(whiteRect);
+
+    m_label.draw(params);
+}
+
+void LoadLevelState::onEvent(utility::Event::EventType type)
+{
+    if(type == utility::Event::Resized)
+        m_label.setPosition(m_screen.getSize().x / 2.f - m_label.getWidth() / 2.f, m_screen.getSize().y / 2.f);
 }
