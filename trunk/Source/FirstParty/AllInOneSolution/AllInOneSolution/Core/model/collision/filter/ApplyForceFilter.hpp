a5bf63a5ddd3c169cc28fdfb50e40aa58bcf861d
BlueCobold <BlueCobold@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-05-16 11:23:09 +0000

    [122] fixed level 7 and added force-fields into it, created the force-filter, changed ball to always be drawn last

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/model/collision/filter/ApplyForceFilter.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef APPLY_FORCE_FILTER_HPP
+#define APPLY_FORCE_FILTER_HPP
+
+#include "ActionFilter.hpp"
+#include "../../Ball.hpp"
+
+class ApplyForceFilter : public ActionFilter
+{
+public:
+
+    ApplyForceFilter(
+        const b2Vec2& force,
+        std::unique_ptr<CollisionFilter> child) :
+            m_force(force),
+            ActionFilter(std::move(child))
+    { }
+
+    virtual ~ApplyForceFilter()
+    { }
+
+    virtual bool shouldCollide(Entity* entityA, Entity* entityB)
+    {
+        bool collides = ActionFilter::shouldCollide(entityA, entityB);
+        if(collides)
+        {
+            if(entityA && entityA->getType() == Entity::Ball)
+                entityA->getBody()->ApplyForce(m_force, entityA->getBody()->GetWorldCenter(), false);
+            else if(entityB && entityB->getType() == Entity::Ball)
+                entityB->getBody()->ApplyForce(m_force, entityB->getBody()->GetWorldCenter(), false);
+        }
+        return collides;
+    }
+
+private:
+
+    b2Vec2 m_force;
+};
+
+#endif // APPLY_FORCE_FILTER_HPP
