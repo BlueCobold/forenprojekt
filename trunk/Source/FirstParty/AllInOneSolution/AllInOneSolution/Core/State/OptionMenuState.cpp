d66161ee20d25203056cbcb21d21c2fe25154aa0
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-07-06 12:59:18 +0000

    [137] Level is now displayed in the background;

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/OptionMenuState.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/OptionMenuState.cpp
@@ -1,12 +1,15 @@
 #include "OptionMenuState.hpp"
 #include "../resources/Config.hpp"
 #include <SFML/Graphics/RectangleShape.hpp>
+#include "../model/Level.hpp"
 
 OptionMenuState::OptionMenuState(sf::RenderWindow& screen, 
                                  ResourceManager& resourceManager, 
                                  Config& config) :
     State(screen, resourceManager, config),
-    m_menu(sf::Vector2f(0, 0), screen, resourceManager, config)
+    m_menu(sf::Vector2f(0, 0), screen, resourceManager, config),
+    m_HUD(resourceManager, config),
+    m_level(nullptr)
 {
 }
 
@@ -17,13 +20,12 @@ OptionMenuState::~OptionMenuState()
 void OptionMenuState::onEnter(const EnterStateInformation* enterInformation, const float time)
 {
     State::onEnter(enterInformation,time);
-
+    
     const EnterOptionStateInformation* info = dynamic_cast<const EnterOptionStateInformation*>(enterInformation);
     m_transitionStateInfo.m_followingState = info->m_comeFromState;
-    m_pauseStateInfo.m_level = info->m_level;
+    m_level = info->m_level;
 
     m_menu.setPosition(sf::Vector2f(m_screen.getSize().x / 2.f - m_menu.getSize().x / 2.f, m_screen.getSize().y / 2.f - m_menu.getSize().y / 2.f));
-
 }
 
 StateChangeInformation OptionMenuState::update(const float time)
@@ -34,6 +36,7 @@ StateChangeInformation OptionMenuState::update(const float time)
 
     if(clicked == OptionMenu::BUTTON_CLOSE)
     {
+        m_pauseStateInfo.m_level = m_level;
         m_transitionStateInfo.m_onEnterInformation = &m_pauseStateInfo;
         m_menu.applyChanges();
         return StateChangeInformation(TransitionStateId, &m_transitionStateInfo);
@@ -48,7 +51,16 @@ void OptionMenuState::draw(const DrawParameter& params)
 
     sf::RectangleShape whiteRect;
     whiteRect.setSize(m_screen.getView().getSize());
-    whiteRect.setFillColor(sf::Color(255, 255, 255, 255));
+    if(m_level != nullptr)
+    {
+        m_level->adjustView(params.getTarget());
+        m_HUD.update(m_level, getCurrentTime());
+        m_level->draw(params);
+        m_HUD.draw(params);
+        whiteRect.setFillColor(sf::Color(255, 255, 255, 128));
+    }
+    else
+        whiteRect.setFillColor(sf::Color(255, 255, 255, 255));
     params.getTarget().draw(whiteRect);
 
     m_menu.draw(params);
