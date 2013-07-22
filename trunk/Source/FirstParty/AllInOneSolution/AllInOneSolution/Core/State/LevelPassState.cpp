3c14d5b8489596b316a05e1dd928a2bcf772554d
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-07-22 13:44:52 +0000

    [156] add possibility to go back in main menu from level pass menu / level fail menu

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/LevelPassState.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/LevelPassState.cpp
@@ -20,10 +20,6 @@ LevelPassState::LevelPassState(sf::RenderWindow& screen,
     m_HUD(resourceManager, config),
     m_replay(false)
 {
-    m_menu.registerOnClick([this](const Button& sender)
-    {
-        m_replay = (sender.getId() == ReplayMenu::BUTTON_PLAY_AGAIN);
-    });
 }
 
 LevelPassState::~LevelPassState()
@@ -55,13 +51,23 @@ StateChangeInformation LevelPassState::update(const float time)
 
     m_HUD.update(m_level, getCurrentTime());
 
+    int clicked = -1;
+    m_menu.registerOnClick([&](const Button& sender){ clicked = sender.getId(); });
     m_menu.update(m_screen);
 
-    if(m_replay)
+    if(clicked == ReplayMenu::BUTTON_PLAY_AGAIN)
     {
         m_playStateInfo.m_returnFromPause = false;
         m_playStateInfo.m_level = m_level;
         m_transitionStateInfo.m_followingState = LoadLevelStateId;
+        m_transitionStateInfo.m_onEnterInformation = &m_playStateInfo;
+        return StateChangeInformation(TransitionStateId, &m_transitionStateInfo);
+    }
+    else if(clicked == ReplayMenu::BUTTON_MAIN_MENU)
+    {
+        m_playStateInfo.m_returnFromPause = false;
+        m_playStateInfo.m_level = nullptr;
+        m_transitionStateInfo.m_followingState = MainMenuStateId;
         m_transitionStateInfo.m_onEnterInformation = &m_playStateInfo;
         return StateChangeInformation(TransitionStateId, &m_transitionStateInfo);
     }
