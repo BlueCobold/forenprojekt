78b27047e5f450b921c8db19867ebe92f6fb4da8
Koschi <Koschi_2@web.de>
2013-07-28 10:39:51 +0000

    [164] add goodyfont and a goody shows now his charges

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/hud/GoodyHUD.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/hud/GoodyHUD.cpp
@@ -1,28 +1,32 @@
-#include "GoodyHUD.hpp"
+﻿#include "GoodyHUD.hpp"
 #include "../../resources/ResourceManager.hpp"
 #include "../../Input.hpp"
 
 #include "../../model/Goody.hpp"
 
 GoodyHUD::GoodyHUD(ResourceManager& resourceManager,
-                     const std::string& iconKey,
-                     const sf::IntRect& textureRect,
-                     const sf::Vector2f& position,
-                     float hReference,
-                     float vReference) :
-    HUDElement(position, 0, hReference, vReference)
+                   const std::string& iconKey,
+                   const sf::IntRect& textureRect,
+                   const sf::Vector2f& position,
+                   float hReference,
+                   float vReference) :
+    HUDElement(position, 0, hReference, vReference),
+    m_charges("", position, 0, resourceManager.getBitmapFont("goodyFont"))
 {
     m_icon.setTexture(*resourceManager.getTexture(iconKey));
     m_icon.setTextureRect(textureRect);
 
     m_startRect = textureRect;
     m_iconHeight = m_startRect.height;
+
+    m_charges.setOffset(sf::Vector2f(5, 20));
 }
 
 void GoodyHUD::update(const DrawParameter& params)
 {
     HUDElement::update(params);
     m_icon.setPosition(getPosition());
+    m_charges.setPosition(getPosition());
 
     if(m_disabled)
     {
@@ -42,11 +46,21 @@ void GoodyHUD::update(const DrawParameter& params)
 void GoodyHUD::draw(const DrawParameter& params)
 {
     params.getTarget().draw(m_icon);
+    m_charges.draw(params);
 }
 
-void GoodyHUD::updateState(const Goody &goody)
+void GoodyHUD::updateState(const Goody& goody)
 {
+    int charges = goody.getCharges();
+
     m_selected = goody.isSelected();
     m_active = goody.isActive();
-    m_disabled = (goody.getCharges() == 0);
+    m_disabled = (charges == 0);
+
+    if(charges == -1)
+        m_charges.setText(utility::toString("#"));
+    else if(charges > 9)
+        m_charges.setText(utility::toString("9+"));
+    else
+        m_charges.setText(utility::toString(goody.getCharges()));
 }
--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/hud/GoodyHUD.hpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/hud/GoodyHUD.hpp
@@ -4,6 +4,7 @@
 #define GOODYHUD_HPP
 
 #include "HUDElement.hpp"
+#include "../LineLabel.hpp"
 #include <SFML/Graphics/Sprite.hpp>
 #include <iostream>
 
@@ -14,24 +15,23 @@ class GoodyHUD : public HUDElement
 public:
 
     GoodyHUD(ResourceManager& resourceManager,
-              const std::string& iconKey,
-              const sf::IntRect& textureRect,
-              const sf::Vector2f& position,
-              float hReference = HUDElement::Left,
-              float vReference = HUDElement::Top);
+             const std::string& iconKey,
+             const sf::IntRect& textureRect,
+             const sf::Vector2f& position,
+             float hReference = HUDElement::Left,
+             float vReference = HUDElement::Top);
 
     void update(const DrawParameter& params);
 
     void draw(const DrawParameter& params);
 
-    void updateState(const Goody &goody);
+    void updateState(const Goody& goody);
 private:
     static const int Deselected = 0;
     static const int Active = 1;
     static const int Selected = 2;
     static const int Disabled = 3;
     static const int DisabledSelected = 4;
-
 
     sf::Sprite m_icon;
     sf::IntRect m_startRect;
@@ -40,6 +40,8 @@ private:
     bool m_selected;
     bool m_disabled;
     bool m_active;
+
+    LineLabel m_charges;
 };
 
 #endif // GOODY_HPP
