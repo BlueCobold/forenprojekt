af888f8943ad2a4b318460dbd3232f7653fbc9ae
BlueCobold <BlueCobold@users.noreply.github.com>
2013-08-01 13:10:04 +0000

    [182] missing file

---
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/MenuElement.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#ifndef MENU_ELEMENT_HPP
+#define MENU_ELEMENT_HPP
+
+#include "../rendering/Drawable.hpp"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace MenuElementType
+{
+    enum Type
+    {
+        Button,
+        CheckBox,
+        Label,
+        Image,
+        Slider,
+        SubWindow
+    };
+}
+
+/// The basic interface type of all menu elements to unify their common attributes
+class MenuElement : public Drawable
+{
+public:
+    
+
+    MenuElement(const int id,
+                const MenuElementType::Type type,
+                const sf::Vector2f& position,
+                const sf::Vector2f& offset) :
+        m_id(id),
+        m_type(type),
+        m_position(position),
+        m_offset(offset)
+    { }
+
+    virtual ~MenuElement()
+    { }
+
+    virtual void update(const sf::RenderWindow& screen)
+    { }
+    virtual void draw(const DrawParameter& params) override = 0;
+    void setPosition(const sf::Vector2f& position)
+    {   
+        if(position != m_position)
+        {
+            m_position = position;
+            onPositionChanged();
+        }
+    }
+
+    void setPosition(const float x, const float y)
+    {
+        setPosition(sf::Vector2f(x, y));
+    }
+
+    int getId() const
+    {
+        return m_id;
+    }
+
+    const MenuElementType::Type getType() const
+    {
+        return m_type;
+    }
+
+    const sf::Vector2f& getPosition() const
+    {
+        return m_position;
+    }
+
+protected:
+
+    virtual void onPositionChanged()
+    { }
+
+    const sf::Vector2f& getOffset()
+    {
+        return m_offset;
+    }
+
+private:
+
+    int m_id;
+    MenuElementType::Type m_type;
+    sf::Vector2f m_position;
+    sf::Vector2f m_offset;
+};
+
+#endif // MENU_ELEMENT_HPP
