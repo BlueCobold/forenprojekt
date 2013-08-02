85b507ad655edc3e9582f017356e32b30af0f363
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-08-02 20:27:34 +0000

    Fixing Bug: game crashes after play a level-> pause-> option menu -> close -> Restart Level

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/OptionMenuState.cpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/OptionMenuState.cpp
@@ -30,6 +30,8 @@ void OptionMenuState::onEnter(const EnterStateInformation* enterInformation, con
 
     m_menu.onEnter();
     m_menu.setPosition(sf::Vector2f(m_screen.getSize().x / 2.f - m_menu.getSize().x / 2.f, m_screen.getSize().y / 2.f - m_menu.getSize().y / 2.f));
+
+    m_pauseStateInfo.m_levelNumber = enterInformation->m_levelNumber;
 }
 
 StateChangeInformation OptionMenuState::update(const float time)
