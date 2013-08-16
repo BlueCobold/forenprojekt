bfe7a42305039b272d629b783ad3183cb9f0bb87
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2013-08-16 18:11:13 +0000

    [204][207][208] 204: Cost and refund defined in CoinMenuShop 207: Add ToolTip for each goody what it is doing 208: Add ToolTip for each + and - what cost or refund it has Will not work until mouse works in SubWindow!!!

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/CoinShopMenu.hpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/CoinShopMenu.hpp
@@ -12,21 +12,38 @@ public:
     static const int BUTTON_CLOSE = 17;
     static const int SPRITE_GRAVITY = 1;
     static const int LABEL_GRAVITY = 2;
+    static const int BUTTON_GRAVITY_PLUS = 3;
+    static const int BUTTON_GRAVITY_MINUS = 4;
     static const int SPRITE_INVULNERABLE = 5;
     static const int LABEL_INVULNERABLE = 6;
+    static const int BUTTON_INVULNERABLE_PLUS = 7;
+    static const int BUTTON_INVULNERABLE_MINUS = 8;
     static const int SPRITE_EXTRA_BALL = 9;
     static const int LABEL_EXTRA_BALL = 10;
+    static const int BUTTON_EXTRA_BALL_PLUS = 11;
+    static const int BUTTON_EXTRA_BALL_MINUS = 12;
     static const int SPRITE_EXTRA_TIME = 13;
     static const int LABEL_EXTRA_TIME = 14;
+    static const int BUTTON_EXTRA_TIME_PLUS = 15;
+    static const int BUTTON_EXTRA_TIME_MINUS = 16;
     
     CoinShopMenu(const sf::Vector2f& position, sf::RenderWindow& screen, ResourceManager& resourceManager);
 
     void setGoodyCharges(const Goody::Type& goody, const int charges);
+    int getBuyCost(const Goody::Type& goody) const;
+    int getSellRefund(const Goody::Type& goody) const;
 
 private:
-
+    // icon
     static const int Deselected = 0;
     static const int Disabled = 3;
+
+    // price
+    static const int Price_Gravity = 1000;
+    static const int Price_Invulnerable = 400;
+    static const int Price_Extra_Ball = 400;
+    static const int Price_Extra_Time = 500;
+    static const float SellModifier;
 
     void setGoodyIcon(const Goody::Type& goody, const int charges);
 };
