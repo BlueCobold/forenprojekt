0a3dfc1603b5ed51e5bb336f311f526094127b90
Koschi <Koschi_2@web.de>
2013-08-16 18:11:13 +0000

    [204][207][208] 204: Cost and refund defined in CoinMenuShop 207: Add ToolTip for each goody what it is doing 208: Add ToolTip for each + and - what cost or refund it has Will not work until mouse works in SubWindow!!!

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/CoinShopMenu.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/CoinShopMenu.cpp
@@ -1,12 +1,38 @@
 #include "CoinShopMenu.hpp"
+#include "Button.hpp"
 
 #include "../resources/ResourceManager.hpp"
+
+const float CoinShopMenu::SellModifier = 0.8f;
 
 CoinShopMenu::CoinShopMenu(const sf::Vector2f& position,
                            sf::RenderWindow& screen,
                            ResourceManager& resourceManager) :
     CaptionMenu(*resourceManager.getMenuTemplate("CoinShopMenu"), position, screen)
 {
+    Menu::getButton(BUTTON_GRAVITY_PLUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_cost"),
+                                                        utility::toString(Price_Gravity)));
+
+    Menu::getButton(BUTTON_GRAVITY_MINUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_refund"),
+                                                         utility::toString(static_cast<int>(Price_Gravity * SellModifier))));
+
+    Menu::getButton(BUTTON_INVULNERABLE_PLUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_cost"),
+                                                             utility::toString(Price_Invulnerable)));
+
+    Menu::getButton(BUTTON_INVULNERABLE_MINUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_refund"),
+                                                              utility::toString(static_cast<int>(Price_Invulnerable * SellModifier))));
+
+    Menu::getButton(BUTTON_EXTRA_BALL_PLUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_cost"),
+                                                           utility::toString(Price_Extra_Ball)));
+
+    Menu::getButton(BUTTON_EXTRA_BALL_MINUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_refund"),
+                                                            utility::toString(static_cast<int>(Price_Extra_Ball * SellModifier))));
+
+    Menu::getButton(BUTTON_EXTRA_TIME_PLUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_cost"),
+                                                           utility::toString(Price_Extra_Time)));
+
+    Menu::getButton(BUTTON_EXTRA_TIME_MINUS).setToolTipText(utility::replace(utility::translateKey("tooltip_goody_refund"),
+                                                            utility::toString(static_cast<int>(Price_Extra_Time * SellModifier))));
 }
 
 void CoinShopMenu::setGoodyCharges(const Goody::Type& goody, const int charges)
@@ -19,6 +45,8 @@ void CoinShopMenu::setGoodyCharges(const Goody::Type& goody, const int charges)
         Menu::getLabel(LABEL_EXTRA_BALL).setText(utility::toString<int>(charges));
     else if(goody == Goody::ExtraTimeGoody)
         Menu::getLabel(LABEL_EXTRA_TIME).setText(utility::toString<int>(charges));
+    else
+        throw std::runtime_error(utility::translateKey("InvalidGoody"));
 
     setGoodyIcon(goody, charges);
 }
@@ -39,4 +67,34 @@ void CoinShopMenu::setGoodyIcon(const Goody::Type& goody, const int charges)
         Menu::getSprite(SPRITE_EXTRA_BALL).setTextureRect(textureRect);
     else if(goody == Goody::ExtraTimeGoody)
         Menu::getSprite(SPRITE_EXTRA_TIME).setTextureRect(textureRect);
+    else
+        throw std::runtime_error(utility::translateKey("InvalidGoody"));
+}
+
+int CoinShopMenu::getBuyCost(const Goody::Type& goody) const
+{
+    if(goody == Goody::GravityGoody)
+        return Price_Gravity;
+    else if(goody == Goody::InvulnerableGoody)
+        return Price_Invulnerable;
+    else if(goody == Goody::ExtraBallGoody)
+        return Price_Extra_Ball;
+    else if(goody == Goody::ExtraTimeGoody)
+        return Price_Extra_Time;
+    else
+        throw std::runtime_error(utility::translateKey("InvalidGoody"));
+}
+
+int CoinShopMenu::getSellRefund(const Goody::Type& goody) const
+{
+    if(goody == Goody::GravityGoody)
+        return static_cast<int>(Price_Gravity * SellModifier);
+    else if(goody == Goody::InvulnerableGoody)
+        return static_cast<int>(Price_Invulnerable * SellModifier);
+    else if(goody == Goody::ExtraBallGoody)
+        return static_cast<int>(Price_Extra_Ball * SellModifier);
+    else if(goody == Goody::ExtraTimeGoody)
+        return static_cast<int>(Price_Extra_Time * SellModifier);
+    else
+        throw std::runtime_error(utility::translateKey("InvalidGoody"));
 }
