80c668a31b9f52b98f41255de04480b1c4601b7c
Koschi <Koschi_2@web.de>
2013-12-10 18:43:26 +0000

    [232] fixed a bug

--- Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/NewHighScoreState.cpp
+++ Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/State/NewHighScoreState.cpp
@@ -150,7 +150,7 @@ void NewHighScoreState::addNewHighScore(int points, std::string name)
             State::m_config.set<std::string>(termName2, State::m_config.get<std::string>(termName1));
         }
         std::string termPoints = "HighScoreLevel" + utility::toString(number) + "_Points" + utility::toString(newPlace) + "NAM";
-        std::string termName = "HighScoreLevel" + utility::toString(number) + "_Name" + utility::toString(newPlace) + "NAM";;
+        std::string termName = "HighScoreLevel" + utility::toString(number) + "_Name" + utility::toString(newPlace) + "NAM";
 
         State::m_config.set<int>(termPoints, points);
         State::m_config.set<std::string>(termName, name);
