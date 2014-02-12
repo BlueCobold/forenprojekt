15e99240a6194f5e61bf597a748c4945cc0ef25d
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2014-02-12 19:36:15 +0000

    [243] correct the code

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/AnimationContainer.hpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/AnimationContainer.hpp
@@ -3,13 +3,14 @@
 #ifndef ANIMATIONCONTAINER_HPP
 #define ANIMATIONCONTAINER_HPP
 
-#include "../animation/AnimatedObject.hpp"
+#include "../model/AnimatedGraphics.hpp"
 #include "MenuElement.hpp"
 
-class AnimationContainer : public AnimatedObject, MenuElement
+class AnimationContainer : public AnimatedGraphics, MenuElement
 {
 private:
     std::map<std::string, float> m_variables;
+    Animation* m_updatingAni;
 public:
     AnimationContainer() :
         MenuElement(-1, MenuElementType::Animation, sf::Vector2f(0, 0), sf::Vector2f(0, 0))
@@ -18,21 +19,41 @@ public:
 
     virtual float getValueOf(const std::string& name) const override
     {
-        auto found = m_variables.find(name);
-        if(found != end(m_variables))
-            return found->second;
-        else
-            throw std::runtime_error(utility::replace(utility::translateKey("NoVariable"), name));
-        return 0;
+        auto match = m_variables.find(name);
+        if(match == end(m_variables))
+        {
+            if(m_updatingAni == nullptr)
+                throw std::runtime_error(utility::replace(utility::translateKey("GetVariable"), name));
+            return m_updatingAni->getValueOf(name);
+        }
+        return match->second;
     }
 
     virtual void setValueOf(const std::string& name, const float value) override
     {
+        if(m_updatingAni == nullptr)
         m_variables[name] = value;
+        else
+        {
+            auto match = m_variables.find(name);
+            if(match == end(m_variables))
+                m_updatingAni->setValueOf(name, value);
+            else
+                m_variables[name] = value;
+        }
     }
 
     void draw(const DrawParameter& param)
     {
+        for(auto animation = begin(getAnimations()); animation != end(getAnimations()); ++animation)
+        {
+            auto ani = (*animation).get();
+            if(ani->isStopped())
+                continue;
+            m_updatingAni = ani;
+            ani->setPosition(utility::toPixel(getPosition().x), utility::toPixel(getPosition().y));
+            ani->update();
+        }
     }
 };
 
