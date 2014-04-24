dd65d2539a8eb4da0d8b47652642e0f96a36e794
Koschi_2@web.de <Koschi_2@web.de@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2014-04-24 19:46:28 +0000

    [256] In release version errors will now displayed in a MessageBox

---
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/ErrorMessageBox.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef ERRORMESSAGEBOX_HPP
+#define ERRORMESSAGEBOX_HPP
+
+#include <Windows.h>
+#include <iostream>
+
+class ErrorMessageBox
+{
+private:
+    std::string m_message;
+
+public:
+    ErrorMessageBox(std::string message) :
+        m_message(message)
+    {
+        show();
+    }
+
+    int show()
+    {
+         return MessageBox(0, m_message.c_str(), "Error:", MB_OK | MB_ICONSTOP);
+    }
+
+};
+#endif
