df34dac2ca277436765cc356a94afb07b1000f10
BlueCobold <BlueCobold@745f6cd2-e543-7d0d-d124-70bd9bff8563>
2015-01-13 15:51:27 +0000

    [248][Bug] New graphics for teeters, online/offline highscore using buttons similar to mode selection

--- trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/HighScoreMenu.hpp
+++ trunk/Source/FirstParty/AllInOneSolution/AllInOneSolution/Core/gui/HighScoreMenu.hpp
@@ -12,7 +12,8 @@ public:
     static const int BUTTON_CLOSE = 0;
     static const int LABEL_PLACES = 11;
     static const int LABEL_POINTS = 21;
-    static const int CHECKBOX_GLOBAL_HIGHSCORE = 26;
+    static const int CHECKBOX_LOCAL_HIGHSCORE = 30;
+    static const int CHECKBOX_GLOBAL_HIGHSCORE = 31;
     static const int LABEL_LOADING = 27;
 
     HighScoreMenu(const sf::Vector2f& position, sf::RenderWindow& screen, ResourceManager& resourceManager);
