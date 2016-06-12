60ce5c115fef68e6da5122cb4fe5165f12a52eb2
BlueCobold <BlueCobold@users.noreply.github.com>
2016-06-12 19:32:41 +0200

    [327] Add new battery icons

--- Source/FirstParty/Src/gui/hud/BatteryHUD.cpp
+++ Source/FirstParty/Src/gui/hud/BatteryHUD.cpp
@@ -9,7 +9,7 @@ BatteryHUD::BatteryHUD(ResourceManager& resourceManager,
     HUDElement(position, offset),
     m_resourceManager(resourceManager)
 {
-    m_icon.setTexture(*m_resourceManager.getTexture("battery"));
+    m_icon.setTexture(*m_resourceManager.getTexture("GuiElements"));
     m_icon.setPosition(getCurrentPosition());
     setIcon();
 }
@@ -34,20 +34,20 @@ void BatteryHUD::setIcon()
     auto batteryState = utility::getBatteryState();
     
     if(batteryState.state == BatteryState::Charging)
-        m_icon.setTextureRect(getIconRect("Charging"));
+        m_icon.setTextureRect(getIconRect("IconBatteryCharge"));
     else if(batteryState.percent > 0.75f)
-        m_icon.setTextureRect(getIconRect("More75"));
+        m_icon.setTextureRect(getIconRect("IconBattery100"));
     else if(batteryState.percent > 0.50f)
-        m_icon.setTextureRect(getIconRect("More50"));
+        m_icon.setTextureRect(getIconRect("IconBattery075"));
     else if(batteryState.percent > 0.25f)
-        m_icon.setTextureRect(getIconRect("More25"));
+        m_icon.setTextureRect(getIconRect("IconBattery050"));
     else if(batteryState.percent < 0.25f)
-        m_icon.setTextureRect(getIconRect("Less25"));
+        m_icon.setTextureRect(getIconRect("IconBattery025"));
 }
 
 sf::IntRect BatteryHUD::getIconRect(const std::string& key)
 {
-    auto sheet = m_resourceManager.getSpriteSheet("gui_battery");
+    auto sheet = m_resourceManager.getSpriteSheet("gui_elements");
     auto data = sheet->get(key);
 
     return sf::IntRect(data.x, data.y, data.width, data.height);
